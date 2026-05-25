705257b06f007779f46603b8b363b3218a06fd6b
Papratt <antea.parat@gmail.com>
2026-05-25 15:03:05 +0200

    updejt Antea

--- antea_pokusaji/samo_senzori.py
+++ antea_pokusaji/samo_senzori.py
@@ -19,17 +19,6 @@ mux_s = [
 # ADC za čitanje MUX izlaza
 adc = ADC(Pin(26))  # IRT_READ
 
-# Motor driver
-left_motor_1  = Pin(4, Pin.OUT)
-left_motor_2  = Pin(5, Pin.OUT)
-right_motor_1 = Pin(6, Pin.OUT)
-right_motor_2 = Pin(7, Pin.OUT)
-
-speed_left  = PWM(Pin(8))
-speed_right = PWM(Pin(9))
-speed_left.freq(1000)           # frekvencija PWM signala (1 kHz je dobar izbor za motore)
-speed_right.freq(1000)          # jer prevelika frekvencija može uzrokovati pregrijavanje motora, a premala može uzrokovati zujanje i neefikasno upravljanje brzinom
-
 PRAG = 30000  # podesi eksperimentalno (0-65535) - vrijednost iznad koje se smatra da je prepreka detektirana
 
 def mux_odaberi(kanal):
@@ -43,22 +32,6 @@ def citaj_senzor(kanal):
     time.sleep_us(50)           # kratka pauza da se MUX stabilizira
     return adc.read_u16()       # to je fototranz na gpio26, koji je povezan na MUX izlaz
 
-#100 000 ne postoji — duty_u16 je 16-bitni, maksimum je 65535. Sve iznad toga se "wraparound" ili baca grešku, ne daje više brzine
-def motori_naprijed(brzina=65535):
-    left_motor_1.value(0)  
-    left_motor_2.value(1)
-      
-    right_motor_1.value(1)
-    right_motor_2.value(0)
-    speed_left.duty_u16(brzina)
-    speed_right.duty_u16(brzina)
-
-def motori_stop():
-    left_motor_1.value(0); left_motor_2.value(0)
-    right_motor_1.value(0); right_motor_2.value(0)
-    speed_left.duty_u16(0)
-    speed_right.duty_u16(0)
-
 # Upali sve IR LEDs
 for led in ir_led:
     led.value(1)
@@ -70,8 +43,6 @@ while True:
     print(f"Senzori: {ocitanja} | {'STOP' if prepreka else 'OK'}")
 
     if prepreka:
-        motori_stop()
-    else:
-        motori_naprijed()
+        print("Prepreka detektirana!")
 
-    time.sleep(0.05)        
+    time.sleep(0.05) 
